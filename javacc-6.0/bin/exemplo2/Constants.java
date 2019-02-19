6d74f483b05ec632ff4864419bd3a87f24746a62
Luis Mendes <luis.alvela@gmail.com>
2019-02-19 17:50:55 +0000

    examples done

---
+++ javacc-6.0/bin/exemplo2/Constants.java
@@ -0,0 +1,7 @@
+public class Constants {
+    public static final int ADD = 1;
+    public static final int SUB = 2;
+    public static final int MUL = 3;
+    public static final int DIV = 4;
+    public static final char[] ops = {'+','-','*','/'};
+}
